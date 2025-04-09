bb76f2ed1ea35b124173f2599db2cf559cd70bb8
Pedro Henrique <kfp3drod15@gmail.com>
2025-04-09 16:34:56 -0300

    Estilo do menu mudado

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,44 +1,80 @@
-import '../../assets/style/menu.css'
+import React, { useState, useEffect } from 'react';
+import '../../assets/style/menu.css';
+
+function Menu() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true';
+  });
+
+  useEffect(() => {
+    if(darkMode) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark');
+      document.body.classList.add('bg-black');
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', 'light');
+      document.body.classList.remove('bg-black');
+    }
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <nav className={`navbar navbar-expand-lg fixed-top ${darkMode ? 'navbar-dark bg-black' : 'navbar-light bg-light'}`}>
+      <div className="container-fluid">
+    
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <button 
+          className="btn btn-link d-lg-none ms-2" 
+          onClick={toggleDarkMode}
+          aria-label={darkMode ? "Modo claro" : "Modo escuro"}
+        >
+          <i className={`bi ${darkMode ? 'bi-sun' : 'bi-moon'} fs-5`}></i>
+        </button>
 
 
-function Menu (params) {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item px-5">
+              <a className={`nav-link ${darkMode ? 'text-white' : 'text-dark'}`} href="#about">
+                Sobre mim
+              </a>
+            </li>
+            <li className="nav-item px-5">
+              <a className={`nav-link ${darkMode ? 'text-white' : 'text-dark'}`} href="#projetos">
+                Projetos
+              </a>
+            </li>
+            <li className="nav-item px-5">
+              <a className={`nav-link ${darkMode ? 'text-white' : 'text-dark'}`} href="#">
+                Contatos
+              </a>
+            </li>
+          </ul>
+          
 
-      <div>
-        <a className="navbar-brand" href="#home">
-          <i className="bi bi-terminal fs-10 text-white"></i>
-        </a>
+          <button 
+            className="btn btn-link d-none d-lg-block" 
+            onClick={toggleDarkMode}
+            aria-label={darkMode ? "Modo claro" : "Modo escuro"}
+          >
+            <i className={`bi ${darkMode ? 'bi-sun' : 'bi-moon'} fs-5`}></i>
+          </button>
+        </div>
       </div>
-
-      <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-
-          <li className="nav-item">
-            <a className="nav-link" href="#about">Sobre mim</a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" href="#projetos">Projetos</a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="#">Contatos</a>
-          </li>
-        </ul>
-
-
-      </div>
-    </div>
-  </nav>
-    );
+    </nav>
+  );
 }
 
 export default Menu;
