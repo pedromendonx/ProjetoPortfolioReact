87a6cc6683090b994c4e588fc1e85ee73fcf481a
Pedro Henrique <kfp3drod15@gmail.com>
2025-04-09 16:36:09 -0300

    background e texto da home atualizados

--- src/components/portfolio/Home.jsx
+++ src/components/portfolio/Home.jsx
@@ -1,20 +1,21 @@
-import "../../assets/style/home.css"
+import "../../assets/style/home.css";
 
-function Home (params) {
-    return(
-        <main> 
-              <section id="home" className="row m-0 p-0 container-fluid">
-        <div className="col-md-5 d-flex justify-content-center align-items-left flex-column">
-          <h1 className="text-white text-shadow text"> Pedro Henrique </h1>
-          <span id="subtitle" className="text-white text-shadow"> Programador full-stack </span>
-  
+function Home() {
+  return (
+    <main>
+      <section id="home" className="row m-0 p-0 container-fluid min-vh-100 position-relative">
+        
+        <div className="col-md-5 d-flex justify-content-center align-items-center flex-column mx-auto py-5 position-relative z-2">
+          <h1 className="text-white text-center text-shadow display-4 mb-3 mt-3">
+            Olá, meu nome é Pedro Henrique.
+          </h1>
+          <span className="text-white text-center text-shadow fs-5 pt-3 pb-3">
+            Sou um programador fullstack, formado em Técnico em Informática, com conhecimento em React.js, Java, MySQL, Git e GitHub.
+          </span>
         </div>
-      </section>  
-
-
-        </main>
-    
-    );
+      </section>
+    </main>
+  );
 }
 
 export default Home;
